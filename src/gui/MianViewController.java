49f555911f622f74f8a1716e9fdce739876f38e5
Valdemir Barbosa <vbarbosa741@gmail.com>
2020-06-01 23:05:40 -0300

    Inicialização com parametro

--- src/gui/MianViewController.java
+++ src/gui/MianViewController.java
@@ -3,17 +3,18 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
 
@@ -35,12 +36,15 @@ public class MianViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml" , (DepartmentListController controller) -> {
+			controller.setDepartmentService (new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml" , x -> {} );
 	}
 
 	@Override
@@ -48,7 +52,7 @@ public class MianViewController implements Initializable {
 		// TODO Auto-generated method stub
 	}
 
-	private synchronized void loadView(String absolutName) {
+	private synchronized <T>void loadView(String absolutName, Consumer<T> initializingAction) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName));
 			VBox newvbox = loader.load();	
@@ -58,29 +62,14 @@ public class MianViewController implements Initializable {
 			mainVbox.getChildren().clear();
 			mainVbox.getChildren().add(mainMenu);
 			mainVbox.getChildren().addAll(newvbox.getChildren());
+			
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 			
 		} catch (IOException e) {
 			Alerts.showAlert("IOexception", "Erro ao carregar página", e.getMessage(), AlertType.ERROR);
 		}
 	}
 
-	private synchronized void loadView2(String absolutName) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName));
-			VBox newvbox = loader.load();	
-			Scene mainScene = Main.getMainScene();
-			VBox mainVbox = (VBox) ((ScrollPane)  mainScene.getRoot()).getContent();
-			Node mainMenu = mainVbox.getChildren().get(0);
-			mainVbox.getChildren().clear();
-			mainVbox.getChildren().add(mainMenu);
-			mainVbox.getChildren().addAll(newvbox.getChildren());
-			
-			DepartmentListController controler = loader.getController();
-			controler.setDepartmentService(new DepartmentService());
-			controler.updateTableView();
-			
-		} catch (IOException e) {
-			Alerts.showAlert("IOexception", "Erro ao carregar página", e.getMessage(), AlertType.ERROR);
-		}
-	}
+	
 }
